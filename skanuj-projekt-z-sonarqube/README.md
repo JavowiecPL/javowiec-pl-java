6aab491d6281c3eb1397d90f80289bf36acfe667
JavowiecPL <aventura.mprzygoda@gmail.com>
2021-12-17 16:23:47 +0100

    Add skanuj-projekt-z-sonarqube module

---
+++ skanuj-projekt-z-sonarqube/README.md
@@ -0,0 +1,9 @@
+# Skanuj projekt z SonarQube
+
+Darmowa aplikacja ułatwiająca poprawę jakości kodu i łatania luk bezpieczeństwa w projekcie zespołowym
+
+## Linki
+
+Blog Javowiec.pl: https://javowiec.pl
+
+Post na blogu: https://javowiec.pl/java/skanuj-projekt-z-sonarqube
---
+++ skanuj-projekt-z-sonarqube/src/main/java/pl/javowiec/SomeConstants.java
@@ -0,0 +1,12 @@
+package pl.javowiec;
+
+/**
+ * Klasa z błędami SonarSource
+ * Patrz: https://rules.sonarsource.com
+ */
+public class SomeConstants {
+
+    public static final String CONSTANT_A = "Constant A";
+    public final static String CONSTANT_b = "Constant B";
+
+}
